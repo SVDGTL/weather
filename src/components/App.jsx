c8b3f4b42802c826bf981535722b9ad25d9091b3
stelsp <stelspeh@gmail.com>
2022-04-29 01:26:21 +0300

    feat: initial

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Test = styled.div`
+  width: 100px;
+  height: 100px;
+  background: pink;
+`;
+
+function App() {
+  return <Test />;
+}
+
+export default App;
